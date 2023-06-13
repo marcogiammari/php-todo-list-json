1d3f9eb43dab789ce0a6f1caec456e6fb10e0ec0
Marco Giammari <marcogiammari@gmail.com>
2023-06-13 17:33:19 +0200

    fix axios

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <title>To Do List</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
         <script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.4.0/axios.min.js" integrity="sha512-uMtXmF28A2Ab/JJO2t/vYhlaa/3ahUOgj1Zf27M5rOo8/+fcTUVH0/E0ll68njmjrLqOBjXM3V9NiPFL5ywWPQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 </head>
 
 <style>
